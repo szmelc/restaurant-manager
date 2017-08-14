61618891b1ea32efa80c8c8b5ae8b589d1c8a5c3
Lukasz Szmelc <lszmelc@gmail.com>
2017-08-14 18:51:54 +0200

    Add relation between orders and users in controlles and view

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,13 @@
 class OrdersController < ApplicationController
+  def index
+    user_id = current_user.id
+    @orders = Order.all
+    @order = @orders.where(:user_id => user_id)
+  end
+
+  def new
+  end
+
+  def create
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
   resources :meals
+  resources :orders
 end
