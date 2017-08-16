346aa183d3bd98419b46fb4d907e11d775f99db0
Lukasz Szmelc <lszmelc@gmail.com>
2017-08-16 22:18:29 +0200

    Tweak order forms so it almost works

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,18 +1,45 @@
 class OrdersController < ApplicationController
+  before_action :find_order, only: [:show]
+  before_action :find_meal, only: [:new]
 
   def index
+    @user = current_user
     user_id = current_user.id
     @userOrders = Order.where(:user_id => user_id)
   end
 
+  def show
+  end
+
   def new
+    @order = current_user.orders.build
+    @meals = Meal.all.map { |meal| [meal.name] }
   end
 
   def create
+    # binding.pry
+    # puts order_params
+    @order = current_user.orders.build(order_params)
+    if @order.save
+      puts "Order saved!"
+    else
+      puts "Something went wrong"
+    end
+    redirect_to orders_path
   end
 
 
   private
+  def order_params
+    params.require(:order).permit(:user_id, meals_attributes: [:name, :id, :price])
+  end
 
+  def find_order
+    @order = Order.find(params[:id])
+  end
+
+  def find_meal
+    @meal = Meal.all
+  end
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :meals
+  accepts_nested_attributes_for :meals
+  # validates :order_id, presence: true
+  # validates_associated :meals
 end
