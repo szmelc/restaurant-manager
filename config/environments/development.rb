ad984b1f80eb0397226f59895d37074a7335c47d
Lukasz Szmelc <lszmelc@gmail.com>
2017-08-21 20:13:03 +0200

    Add paperclip gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,4 +54,7 @@ Rails.application.configure do
 
   # Needed for devise gem
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # Needed for paperclip gem
+  Paperclip.options[:command_path] = "/usr/local/bin/"
 end
