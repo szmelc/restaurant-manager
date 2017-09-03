c5f5cbb705fb978330e68465d86204ec6f23e761
Lukasz Szmelc <lszmelc@gmail.com>
2017-09-03 21:38:58 +0200

    Add abilities

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170826130754) do
+ActiveRecord::Schema.define(version: 20170903184641) do
 
   create_table "dishes", force: :cascade do |t|
     t.string "name"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 20170826130754) do
     t.text "description"
     t.string "city"
     t.string "phonenumber"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
