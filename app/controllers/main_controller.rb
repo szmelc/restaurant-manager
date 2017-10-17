cd92a76ef9310b4fcbd741b3e9c6df85eb0104d7
Lukasz Szmelc <lszmelc@gmail.com>
2017-10-17 14:25:37 +0200

    Improve search feature

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,13 +5,11 @@ class MainController < ApplicationController
 
 	def search
 		@users = User.ransack(name_cont: params[:q]).result(distinct: true)
-		@dishes = Dish.ransack(name_cont: params[:q]).result(distinct: true)
 
 		respond_to do |format|
 			format.html {}
 			format.json {
 				@users = @users.limit(5)
-				@dishes = @dishes.limit(5)
 			}
 		end
 	end
