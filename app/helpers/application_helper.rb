224b7560e3dfb1004ec4ba981fb2b940b8877334
Lukasz Szmelc <lszmelc@gmail.com>
2017-10-23 19:19:44 +0200

    Modify helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,7 +15,7 @@ module ApplicationHelper
 	end
 
 	def average_order_value
-		if income_today != '0 pln'
+		if income_today != '0 pln' && income_today != nil?
 			number_with_precision(income_today / @orders_today.count, precision: 2) + ' pln'
 		else
 			return 0.to_s + ' pln'
