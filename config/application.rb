734e16f14bb9969fdd14bd85708b04c5a03d55d8
Lukasz Szmelc <lszmelc@gmail.com>
2017-10-26 16:44:34 +0200

    Change Language

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Restaurant
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.i18n.default_locale = :pl
+    config.i18n.default_locale = :en
     config.active_record.default_timezone = :local
     config.time_zone = 'Berlin'
 
