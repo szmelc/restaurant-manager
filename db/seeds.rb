34517c85fb19a2170929ee1cab6cef7648c66e35
szmelc <lszmelc@gmail.com>
2018-01-12 12:22:21 +0100

    Add more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
 # Create admin
 admin = User.create(first_name: 'Marek', last_name: 'Mikołajczyk', email: 'legolas@example.com', password: 'legolas123', password_confirmation: 'legolas123', description: 'Ja tu jestem szeryfem.', city: 'Poznań', admin: true )
 
@@ -6,7 +14,7 @@ user = User.create(first_name: 'Jarek', last_name: 'Kanarek', email: 'jarek@exam
 
 # Create dish
 
-Dish.create(name: 'Shrimp', price: '20', description: 'Perfect shrimps oh my god! come to brazil', category: 'Seafood')
+Dish.create(name: 'Shrimps', price: '20', description: 'Perfect shrimps oh my god! come to brazil', category: 'Seafood')
 Dish.create(name: 'Meatballs', price: '30', description: 'Meatballs om nom nom', category: 'Beef')
 Dish.create(name: 'Scrambled Eggs', price: '10', description: 'Scrambled eggs Gordon Ramsay style!', category: 'Breakfasts')
 
@@ -17,3 +25,11 @@ PinnedPost.create(content: 'Radziejów miasto złodziejów.', user: admin)
 # Create post
 
 Post.create(content: 'Jestem knajtem i wale axa :)',  user: user)
+
+# Create meals
+
+meals = Meal.create([{ name: 'Scrambled Eggs', price: 10 }, { name: 'Meatballs', price: 20 }])
+
+# Create order
+
+Order.create(user: admin, meals: meals)
